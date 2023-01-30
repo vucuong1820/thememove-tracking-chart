c18b560dba6e2b09b1c9e83afe57ac4f8d526f89
Vu Manh Cuong <vucuong1820@gmail.com>
2023-01-30 16:32:32 +0700

    coding

--- src/configs/connectMongo.js
+++ src/configs/connectMongo.js
@@ -9,6 +9,7 @@ async function connectMongo() {
   }
   // eslint-disable-next-line no-console
   console.log('ENV:', process.env.MONGO_URI);
+  mongoose.set('strictQuery', true);
   const db = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
