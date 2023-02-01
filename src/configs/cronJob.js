129bf22dd53291e7cb23f0579d81f8e79e439bf7
Vu Manh Cuong <vucuong1820@gmail.com>
2023-02-01 14:10:24 +0700

    auto crawling

--- src/configs/cronJob.js
+++ src/configs/cronJob.js
@@ -1,15 +1,11 @@
-import { LIMIT } from '@constants';
 import themeMoveThemes, { otherThemes } from '@constants/themes';
 import crawlThemes from '@helpers/crawlThemes';
 import { Inngest } from 'inngest';
 
 const inngest = new Inngest({ name: 'thememove-tracking-chart' });
 
-export default inngest.createScheduledFunction('AUTO CRAWL THEMEMOVE THEMES ', 'TZ=America/New_York * * * * *', async (index) => {
-  const start = Number.parseInt(index);
+export default inngest.createScheduledFunction('AUTO-CRAWLING', 'TZ=America/New_York * * * * *', async () => {
   const themeList = [...themeMoveThemes, ...otherThemes];
-
-  const themes = themeList.slice(start * LIMIT, start * LIMIT + LIMIT);
-  await crawlThemes(themes);
-  return `CRAWL SUCCESSFULLY: ${themes.join(', ')}`;
+  await crawlThemes(themeList);
+  return `CRAWL SUCCESSFULLY: ${themeList.join(', ')}`;
 });
--- src/pages/api/auto-crawling/index.js
+++ src/pages/api/auto-crawling/index.js
@@ -1,15 +1,20 @@
-import themeMoveThemes, { otherThemes } from '@constants/themes';
-import crawlThemes from '@helpers/crawlThemes';
+// import themeMoveThemes, { otherThemes } from '@constants/themes';
+// import crawlThemes from '@helpers/crawlThemes';
 
-const LIMIT = 5;
+// const LIMIT = 5;
 
-export default async function handler(req, res) {
-  const { index } = req.query;
-  const start = Number.parseInt(index);
-  const themeList = [...themeMoveThemes, ...otherThemes];
+// export default async function handler(req, res) {
+//   const { index } = req.query;
+//   const start = Number.parseInt(index);
+//   const themeList = [...themeMoveThemes, ...otherThemes];
 
-  const themes = themeList.slice(start * LIMIT, start * LIMIT + LIMIT);
+//   const themes = themeList.slice(start * LIMIT, start * LIMIT + LIMIT);
 
-  await crawlThemes(themes);
-  res.status(200).send(`CRAWL SUCCESSFULLY: ${themes?.map((x) => x?.name)?.join(',')}`);
-}
+//   await crawlThemes(themes);
+//   res.status(200).send(`CRAWL SUCCESSFULLY: ${themes?.map((x) => x?.name)?.join(',')}`);
+// }
+
+import { serve } from 'inngest/next';
+import handleCronJob from '@configs/cronJob';
+
+export default serve('thememove-tracking-chart', [handleCronJob]);
