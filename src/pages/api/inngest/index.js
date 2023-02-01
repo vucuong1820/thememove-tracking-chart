e0a4448c4524a6bb2ad5c3d9992d5a316ed1a5fd
Vu Manh Cuong <vucuong1820@gmail.com>
2023-02-01 14:13:47 +0700

    update

--- src/pages/api/inngest/index.js
+++ src/pages/api/inngest/index.js
@@ -1,13 +1,13 @@
-import { serve } from 'inngest/next';
-import handleCronJob from '@configs/cronJob';
-import themeMoveThemes, { otherThemes } from '@constants/themes';
-import { LIMIT } from '@constants';
+// import { serve } from 'inngest/next';
+// import handleCronJob from '@configs/cronJob';
+// import themeMoveThemes, { otherThemes } from '@constants/themes';
+// import { LIMIT } from '@constants';
 
-const listFunction = () => {
-  const themeList = [...themeMoveThemes, ...otherThemes];
+// const listFunction = () => {
+//   const themeList = [...themeMoveThemes, ...otherThemes];
 
-  return Array.from({ length: Math.ceil(themeList.length / LIMIT) }).map((item, index) => {
-    return () => handleCronJob(index);
-  });
-};
-export default serve('thememove-tracking-chart', listFunction);
+//   return Array.from({ length: Math.ceil(themeList.length / LIMIT) }).map((item, index) => {
+//     return () => handleCronJob(index);
+//   });
+// };
+// export default serve('thememove-tracking-chart', listFunction);
