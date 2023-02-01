b95399a0db70d06a4fa1305d7c3ac6a28f8d4427
Vu Manh Cuong <vucuong1820@gmail.com>
2023-02-01 14:01:46 +0700

    update auto crawl

--- src/configs/cronJob.js
+++ src/configs/cronJob.js
@@ -5,11 +5,11 @@ import { Inngest } from 'inngest';
 
 const inngest = new Inngest({ name: 'thememove-tracking-chart' });
 
-export default inngest.createScheduledFunction('AUTO CRAWL THEMES', 'TZ=America/New_York * * * * *', async (index) => {
+export default inngest.createScheduledFunction('AUTO CRAWL THEMEMOVE THEMES ', 'TZ=America/New_York * * * * *', async (index) => {
   const start = Number.parseInt(index);
   const themeList = [...themeMoveThemes, ...otherThemes];
 
   const themes = themeList.slice(start * LIMIT, start * LIMIT + LIMIT);
   await crawlThemes(themes);
-  return `CRAWL SUCCESSFULLY!`;
+  return `CRAWL SUCCESSFULLY: ${themes.join(', ')}`;
 });
