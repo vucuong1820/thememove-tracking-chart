fd8c69a28c4a4452480510edc28d842e6daf9aa6
Vu Manh Cuong <vucuong1820@gmail.com>
2023-02-01 14:54:02 +0700

    update cron

--- src/configs/inngestCronJob.js
+++ src/configs/inngestCronJob.js
@@ -1,11 +1,19 @@
+import { LIMIT } from '@constants';
 import themeMoveThemes, { otherThemes } from '@constants/themes';
 import crawlThemes from '@helpers/crawlThemes';
 import { Inngest } from 'inngest';
 
 const inngest = new Inngest({ name: 'thememove-tracking-chart' });
 
-export default inngest.createScheduledFunction('AUTO-CRAWLING', 'TZ=America/New_York * * * * *', async () => {
-  const themeList = [...themeMoveThemes, ...otherThemes];
-  await crawlThemes(themeList);
-  return `CRAWL SUCCESSFULLY: ${themeList.join(', ')}`;
-});
+const handleCron = (index) =>
+  inngest.createScheduledFunction(`AUTO-CRAWLING-${index}`, 'TZ=America/New_York */5 * * * *', async () => {
+    const start = Number.parseInt(index);
+    const themeList = [...themeMoveThemes, ...otherThemes];
+
+    const themes = themeList.slice(start * LIMIT, start * LIMIT + LIMIT);
+
+    await crawlThemes(themes);
+    return `CRAWL SUCCESSFULLY: ${themes?.map((x) => x?.name)?.join(',')}`;
+  });
+
+export default handleCron;
--- src/pages/api/inngest/index.js
+++ src/pages/api/inngest/index.js
@@ -1,4 +1,10 @@
 import { serve } from 'inngest/next';
 import handleCronJob from '@configs/inngestCronJob';
+import themeMoveThemes, { otherThemes } from '@constants/themes';
+import { LIMIT } from '@constants';
 
-export default serve('thememove-tracking-chart', [handleCronJob]);
+const listFunction = () => {
+  const themes = [...themeMoveThemes, ...otherThemes];
+  return Array.from({ length: Math.ceil(themes.length / LIMIT) }).map((item, index) => handleCronJob(index));
+};
+export default serve('thememove-tracking-chart', listFunction());
