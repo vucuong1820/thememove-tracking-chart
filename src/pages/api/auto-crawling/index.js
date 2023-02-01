fb487f6aae7566e5a2b9674474050e36db44859a
Vu Manh Cuong <vucuong1820@gmail.com>
2023-02-01 09:17:22 +0700

    update auto crawl

--- src/pages/api/auto-crawling/index.js
+++ src/pages/api/auto-crawling/index.js
@@ -1,7 +1,7 @@
 import themeMoveThemes, { otherThemes } from '@constants/themes';
 import crawlThemes from '@helpers/crawlThemes';
 
-const LIMIT = 10;
+const LIMIT = 5;
 
 export default async function handler(req, res) {
   const { index } = req.query;
