71fc7a7d4762312c3ed4631144c5e93e88cbaa4c
Vu Manh Cuong <vucuong1820@gmail.com>
2023-02-01 10:39:30 +0700

    update UI

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,35 +1,60 @@
 import { AppFrame } from '@components/layouts/AppFrame';
 import themeMoveThemes, { otherThemes } from '@constants/themes';
 import manualCrawlService from '@services/manualCrawlService';
-import { AppProvider } from '@shopify/polaris';
+import { AppProvider, Banner, Spinner } from '@shopify/polaris';
 import '@shopify/polaris-viz/build/esm/styles.css';
 import '@shopify/polaris/build/esm/styles.css';
 import en from '@shopify/polaris/locales/en.json';
 import '@styles/globals.css';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 const PolarisVizProvider = dynamic(() => import('@shopify/polaris-viz').then((module) => module.PolarisVizProvider), { ssr: false });
+
+const BannerStyled = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  .Polaris-Banner {
+    width: 400px;
+  }
+`;
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    manualCrawl();
+    (async () => {
+      await manualCrawl();
+      setLoading(false);
+    })();
   }, []);
-  const manualCrawl = () => {
+  const manualCrawl = async () => {
+    const promise = [];
     for (const theme of [...themeMoveThemes, ...otherThemes]) {
-      manualCrawlService(theme.themeId);
+      promise.push(manualCrawlService(theme.themeId));
     }
+    await Promise.allSettled(promise);
   };
   return (
     <AppProvider i18n={en}>
       <PolarisVizProvider>
         <BrowserRouter>
-          <AppFrame>
-            <Head>
-              <title>ThemeMove Tracking</title>
-            </Head>
-            <Component {...pageProps} />
-          </AppFrame>
+          {loading ? (
+            <BannerStyled>
+              <Banner title="Crawling themes..." status="info">
+                <Spinner size="small" />
+              </Banner>
+            </BannerStyled>
+          ) : (
+            <AppFrame>
+              <Head>
+                <title>ThemeMove Tracking</title>
+              </Head>
+              <Component {...pageProps} />
+            </AppFrame>
+          )}
         </BrowserRouter>
       </PolarisVizProvider>
     </AppProvider>
