0c5dea06f39b6ee146d065014d1dab54d5629678
Vu Manh Cuong <vucuong1820@gmail.com>
2023-02-15 09:32:39 +0700

    update chart

--- src/components/layouts/PolarisLineChart.jsx
+++ src/components/layouts/PolarisLineChart.jsx
@@ -1,9 +1,21 @@
 import dynamic from 'next/dynamic';
+import styled from 'styled-components';
 
 const LineChart = dynamic(() => import('@shopify/polaris-viz').then((module) => module.LineChart), { ssr: false });
 
+const Container = styled.div`
+  height: 100%;
+  position: relative;
+  div[data-tooltip='true'] {
+    z-index: 99;
+  }
+`;
 function PolarisLineChart(props) {
-  return <LineChart {...props} theme="Light" />;
+  return (
+    <Container>
+      <LineChart {...props} theme="Light" />;
+    </Container>
+  );
 }
 
 export default PolarisLineChart;
